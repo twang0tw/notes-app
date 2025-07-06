e1d384edec2664181bbee9f6f1014c7b2963d994
Bobby Wang <68357754+twang0tw@users.noreply.github.com>
2025-07-06 15:44:11 -0400

    UI update

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(10, "10 s"),
+  limiter: Ratelimit.slidingWindow(6, "10 s"),
 });
 
 export default ratelimit;
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,19 +5,14 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 const app = express();
 
-connectDB();
-
 // middleware
 app.use(express.json());
 app.use(rateLimiter);
 
-app.use((req, res, next) => {
-  console.log("We just got a new request");
-  next();
-});
-
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-  console.log("Server start on PORT: 5001");
+connectDB().then(() => {
+  app.listen(5001, () => {
+    console.log("Server start on PORT: 5001");
+  });
 });
